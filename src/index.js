659f3f569c46bb4989761e4b72e565e0c020e5b1
meharog <mexarog@gmail.com>
2020-03-20 03:42:31 +0300

    init: start Morse code decoder task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let decExpr = '';
+    for (let i = 0; i < expr.length; i += 10) {
+        let subExpr = expr.substr(i,10);
+        if (subExpr === '**********') {
+            decExpr = decExpr + ' ';
+        } else {
+            let morseWord = '';
+            for (let k = 0; k < subExpr.length; k += 2) {
+                let letterExpr = subExpr.substr(k,2);
+                switch (letterExpr) {
+                    case '10': morseWord = morseWord + '.';
+                    break;
+                    case '11': morseWord = morseWord + '-';
+                    break;
+                };
+            };
+            decExpr = decExpr + MORSE_TABLE[morseWord];
+        };
+    };
+    return decExpr
+};
 
 module.exports = {
     decode
